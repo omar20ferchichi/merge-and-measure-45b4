21e3fbf5cfef76912fcfb7f485ef52b02c15b07e
omar20ferchichi <75736359+omar20ferchichi@users.noreply.github.com>
2026-08-22 14:53:14 +0100

    chore: initialize Expo scaffold

---
+++ App.tsx
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>merge-and-measure-45b4</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({ container: { flex: 1, alignItems: 'center', justifyContent: 'center' } });
